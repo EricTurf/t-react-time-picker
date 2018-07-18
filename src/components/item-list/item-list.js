f091ec7c6c7c16470176d848a9461cedaf36aca5
ericturf <eric.turriff@gmail.com>
2018-07-18 11:55:14 -0400

    removed clog

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -3,18 +3,15 @@ import React from 'react';
 import { Item, List } from './item-list.styled';
 
 export default ({ items, onClick, selectedValue }) => (
-  console.log(selectedValue),
-  (
-    <List>
-      {items.map((v, i) => (
-        <Item
-          selected={v === selectedValue}
-          key={`${v}-${i}`}
-          onClick={() => onClick(v)}
-        >
-          {v}
-        </Item>
-      ))}
-    </List>
-  )
+  <List>
+    {items.map((v, i) => (
+      <Item
+        selected={v === selectedValue}
+        key={`${v}-${i}`}
+        onClick={() => onClick(v)}
+      >
+        {v}
+      </Item>
+    ))}
+  </List>
 );
